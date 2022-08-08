132f0a44919fd9a96bbb88193c19486793f832a3
DESKTOP-ENDAPE0\admin <ljw9611@gmail.com>
2022-08-08 23:51:03 +0900

    초기 커밋.

---
+++ HelloWorld/HelloWorld2.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWorld2
+{
+    class HelloWorld2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!222");
+        }
+    }
+}
